4cb3a82eef4a71ed04f4cf302eb9385e8da7c43b
tposejank <81495861+tposejank@users.noreply.github.com>
2024-07-25 12:49:16 -0400

    Things

---
+++ setup/unix.sh
@@ -0,0 +1,7 @@
+haxelib install lime
+haxelib install openfl
+haxelib install flixel
+haxelib install json2object 3.11.0
+haxelib install grig.midi 0.0.11
+haxelib install format
+haxelib install hxp
