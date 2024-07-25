e05374aecbe66ce39f06b12b42b03a34b8efcc26
tposejank <81495861+tposejank@users.noreply.github.com>
2024-07-25 12:10:05 -0400

    why

---
+++ windows.bat
@@ -0,0 +1,8 @@
+@echo off
+haxelib install lime 8.1.2
+haxelib install openfl 9.3.3
+haxelib install flixel 5.6.1
+haxelib install json2object 3.11.0
+haxelib install grig.midi 0.0.11
+haxelib install format
+haxelib install hxp
