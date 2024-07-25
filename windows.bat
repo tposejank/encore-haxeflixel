708bfd0243098059296d92e3adf8dc6acb6aa13d
tposejank <81495861+tposejank@users.noreply.github.com>
2024-07-25 12:38:53 -0400

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -1,7 +1,7 @@
 @echo off
 haxelib install lime
 haxelib install openfl
-haxelib install flixel 5.8.1
+haxelib install flixel
 haxelib install json2object 3.11.0
 haxelib install grig.midi 0.0.11
 haxelib install format
